ae065b2f4db7deadd7e6edc17a229f32d6f10849
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-31 15:28:09 -0300

    Added payment method with credit card.

--- payments/urls.py
+++ payments/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('bank-billet/', views.bank_billet, name='bank-billet'),
     path('carnet/', views.carnet, name='carnet'),
+    path('credit-card/', views.credit_card, name='credit-card'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -67,3 +67,44 @@ def carnet(request):
         response = gn.create_carnet(params=None, body=body)
         carnet_link = response['data']['carnet_link']
         return redirect(carnet_link)
+
+
+def credit_card(request):
+    if request.method == 'POST':
+        token = request.POST.get('token')
+        data = {
+            'items': [
+                {
+                    'name': 'Course Programming',
+                    'value': 25000,
+                    'amount': 1,
+                }
+            ],
+        }
+        body = {
+            'payment': {
+                'credit_card': {
+                    'installments': 4,
+                    'payment_token': token,
+                    'billing_address': {
+                        'street': "Av. JK",
+                        'number': 909,
+                        'neighborhood': "Bauxita",
+                        'zipcode': "35400000",
+                        'city': "Ouro Preto",
+                        'state': "MG"
+                    },
+                    'customer': {
+                        'name': 'Gorbadoc Oldbuck',
+                        'cpf': '04267484171',
+                        'phone_number': '5144916523',
+                        'birth': "1977-01-15",
+                        'email': 'exemple@gmail.com',
+                    },
+                }
+            },
+            'items': data['items'],
+        }
+        response = gn.create_charge_onestep(params=None, body=body)
+        print(response)
+        return redirect('home')
